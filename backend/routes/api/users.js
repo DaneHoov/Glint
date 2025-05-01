8504f10e9bd1480132b60d156fd66d2ba767c910
danehoov <DaneHoov22@gmail.com>
2025-05-01 14:06:01 -0500

    firstName lastName attributes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -17,6 +17,14 @@ const validateSignup = [
     .isLength({ min: 4 })
     .withMessage("Please provide a username with at least 4 characters."),
   check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1, max: 30 })
+    .withMessage("First name is required and must be less than 30 characters."),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1, max: 30 })
+    .withMessage("Last name is required and must be less than 30 characters."),
   check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
@@ -26,12 +34,20 @@ const validateSignup = [
 
 // Sign up
 router.post("/", validateSignup, async (req, res) => {
-  const { email, password, username } = req.body;
+  const { email, password, username, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({ email, username, hashedPassword });
+  const user = await User.create({
+    email,
+    username,
+    hashedPassword,
+    firstName,
+    lastName,
+  });
 
   const safeUser = {
     id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
     email: user.email,
     username: user.username,
   };

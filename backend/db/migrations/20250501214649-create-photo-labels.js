73bc9ad89a65e81338e9d54a496399a504709ca9
danehoov <DaneHoov22@gmail.com>
2025-05-02 11:14:19 -0500

    photolabels mod/mig

--- backend/db/migrations/20250501214649-create-photo-labels.js
+++ backend/db/migrations/20250501214649-create-photo-labels.js
@@ -1,31 +1,45 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Photo_Labels', {
+    await queryInterface.createTable("PhotoLabels", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       photo_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Photos",
+          key: "id",
+        },
+        onDelete: "CASCADE",
       },
       label_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Labels",
+          key: "id",
+        },
+        onDelete: "CASCADE",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Photo_Labels');
-  }
+    await queryInterface.dropTable("PhotoLabels");
+  },
 };
---
+++ backend/db/models/photolabels.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class PhotoLabels extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      PhotoLabels.belongsTo(models.Photos, { foreignKey: "photo_id" });
+      PhotoLabels.belongsTo(models.Labels, { foreignKey: "label_id" });
+    }
+  }
+  PhotoLabels.init(
+    {
+      photo_id: DataTypes.INTEGER,
+      label_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "PhotoLabels",
+      tableName: "photolabels",
+      underscored: true,
+    }
+  );
+  return PhotoLabels;
+};

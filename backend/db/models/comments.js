58a63071dd9b581217e115130dfc453d1563bab3
danehoov <DaneHoov22@gmail.com>
2025-05-02 11:28:22 -0500

    comments mod/mig

--- backend/db/models/comments.js
+++ backend/db/models/comments.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Comments extends Model {
     /**
@@ -10,17 +8,23 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Comments.belongsTo(models.Photos, { foreignKey: "photo_id" });
+      Comments.belongsTo(models.Users, { foreignKey: "user_id" });
     }
   }
-  Comments.init({
-    id: DataTypes.INTEGER,
-    photo_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER,
-    content: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Comments',
-  });
+  Comments.init(
+    {
+      id: DataTypes.INTEGER,
+      photo_id: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER,
+      content: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Comments",
+      tableName: "comments",
+      underscored: true,
+    }
+  );
   return Comments;
 };

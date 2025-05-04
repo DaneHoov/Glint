d9f0af1a3d31c298f39779de68345914111cc098
danehoov <DaneHoov22@gmail.com>
2025-05-04 14:54:48 -0500

    photo labels seeders

--- backend/db/seeders/20250502164822-demo-photo-labels.js
+++ backend/db/seeders/20250502164822-demo-photo-labels.js
@@ -1,25 +1,43 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("PhotoLabels", [
+      {
+        photo_id: 1, // Beach sunset
+        label_id: 1, // Nature
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        photo_id: 1, // Beach sunset
+        label_id: 4, // Travel
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        photo_id: 2, // Skiing adventure
+        label_id: 1, // Nature
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        photo_id: 2, // Skiing adventure
+        label_id: 2, // Adventure
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        photo_id: 3, // Concert night
+        label_id: 3, // Concert
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("PhotoLabels", null, {});
+  },
 };

85d641c9f0623885c7fe6107b1e5d35aab03af78
danehoov <DaneHoov22@gmail.com>
2025-05-04 13:52:26 -0500

    photo seeders

--- backend/db/seeders/20250502164731-demo-photos.js
+++ backend/db/seeders/20250502164731-demo-photos.js
@@ -1,25 +1,38 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("photos", [
+      {
+        userId: 1,
+        albumId: 1,
+        url: "https://unsplash.com/photos/white-and-black-car-on-brown-sand-during-daytime-wtBex4wQw60",
+        title: "Beach sunset",
+        description: "car shoot",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        url: "https://unsplash.com/photos/a-man-riding-skis-down-a-snow-covered-slope-A7OE4Rxk0qA",
+        title: "Skiing adventure",
+        description: "Fresh powder on the slopes",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 2,
+        url: "https://unsplash.com/photos/stage-light-front-of-audience-NYrVisodQ2M",
+        title: "Concert night",
+        description: "encore",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("photos", null, {});
+  },
 };

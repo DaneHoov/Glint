8b75048ea165cf0af28e29ec0caf99a4110277e3
danehoov <DaneHoov22@gmail.com>
2025-05-07 19:33:36 -0500

    caps fix

--- backend/db/models/photolabels.js
+++ backend/db/models/photolabels.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "PhotoLabels",
-      tableName: "Photolabels",
+      tableName: "PhotoLabels",
     }
   );
   return PhotoLabels;

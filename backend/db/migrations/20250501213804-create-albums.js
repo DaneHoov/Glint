2d94085d3f3e9d6d7dd453c7a6077fa763bf435a
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-05-18 18:26:11 -0400

    modify schema params

--- backend/db/migrations/20250501213804-create-albums.js
+++ backend/db/migrations/20250501213804-create-albums.js
@@ -1,54 +1,52 @@
-"use strict";
-
-let options = {};
+const options = {};
 if (process.env.NODE_ENV === "production") {
-  options.schema = process.env.SCHEMA;
+	options.schema = process.env.SCHEMA;
 }
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(
-      { tableName: "Albums", schema: options.schema },
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        user_id: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: { tableName: "Users", schema: options.schema },
-            key: "id",
-          },
-          onDelete: "CASCADE",
-        },
-        title: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        description: {
-          type: Sequelize.STRING,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        },
-      },
-      options
-    );
-  },
-  async down(queryInterface, Sequelize) {
-    options.tableName = "Albums";
-    await queryInterface.dropTable(options);
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable(
+			"Albums",
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
+				},
+				user_id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					references: {
+						model: "Users",
+						key: "id",
+					},
+					onDelete: "CASCADE",
+				},
+				title: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				description: {
+					type: Sequelize.STRING,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+				},
+			},
+			options,
+		);
+	},
+	async down(queryInterface, Sequelize) {
+		options.tableName = "Albums";
+		await queryInterface.dropTable(options);
+	},
 };

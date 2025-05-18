32d7604ff4d51927c3a400c9772fd1e57c603f0c
danehoov <DaneHoov22@gmail.com>
2025-05-18 17:32:14 -0500

    csrf credentials options

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -17,6 +17,7 @@ export async function csrfFetch(url, options = {}) {
     }
     options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
   }
+  options.credentials = "include";
   // call the default window's fetch with the url and the options passed in
   const res = await window.fetch(url, options);
 
